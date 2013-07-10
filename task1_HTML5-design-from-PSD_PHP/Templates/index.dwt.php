bfbaae61e5f07717bbab9f561d2baf29cbf625d7
Kestutis ITDev <kestutis.itsolutions@gmail.com>
2013-07-09 22:57:32 -0700

    Visual Task 1 completed - PSD to HTML5/CSS3

---
+++ task1_HTML5-design-from-PSD_PHP/Templates/index.dwt.php
@@ -0,0 +1,200 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+<link href="css/styles.css" rel="stylesheet" type="text/css" media="all">
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+<div class="header_container">
+  <header>
+  	<div class="header_container">
+        <div class="logo">
+            <a href="#"><img src="images/logo.png" alt="Credit Sesame" title="Credit Sesame" width="198" height="58" id="logo" style="display:block;" /></a>
+        </div>
+        <div class="header_content">
+        	<div class="user_block">
+            	<button type="button" class="sprites_button secure">&nbsp;</button>
+                <button type="button" class="sprites_button login" onClick="location.href='/login';">&nbsp;</button>
+                <button type="button" class="sprites_button singup" onClick="location.href='/singup';">&nbsp;</button>
+            </div>
+            <nav>
+              <ul>
+                <li><a href="#" class="open">how we help</a></li>
+                <li><a href="#">how it works</a></li>
+                <li><a href="#">mortgage</a></li>
+                <li><a href="#">blog</a></li>
+              </ul>
+            </nav>
+         </div>
+     </div>
+  </header>
+</div>
+<div class="container">
+  <div class="content_notes">
+  	<a href="#">It&#39;s Free! Get Started!</a>
+  </div>	
+  <div class="sidebar1">
+	<div class="open_panel_title">
+    	<h2>How We Help</h2>
+    </div>
+	<div class="closed_panel_title">
+    	<h2>Frequently Asked Questions</h2>
+    </div>
+    <div class="panel_content">
+        <!-- Nothing -->
+    <!-- end .panel_content --></div>
+    <div class="panel_title">
+    	<h2>From our blog</h2>
+    </div>
+  	<div class="panel_content">
+        <nav class="blogs">
+          <ul>
+            <li><a href="#">How Cleaning House Can Put Cash in Your Pocket</a></li>
+            <li><a href="#">This Week in Money Management: Where Does Your Tax Money Go?</a></li>
+            <li><a href="#">How to Negotiate Your Debt Down</a></li>
+            <li><a href="#">How Mortgage Defaults Impact FICO Scores</a></li>
+          </ul>
+        </nav>
+    <!-- end .panel_content --></div>
+    <div class="panel_title">
+        <h2>Feedback</h2>
+    </div>
+    <div class="panel_content">
+        <aside>
+          <p>Have an Idea, Problem, or Praise? We would like to hear from you. Please send feedback to: <a href="#">fb@creditsesame.com</a></p>
+        </aside>
+    <!-- end .panel_content --></div>
+    <div class="panel_title">
+    	<h2>Get the Scoop</h2>
+    </div>
+  	<div class="panel_content">
+        <nav class="follow">
+          <ul>
+            <li><a href="#" class="twitter">Follow @ Twitter</a></li>
+            <li><a href="#" class="facebook">Follow @ Facebook</a></li>
+            <li><a href="#" class="feed">Visit our Blog</a></li>
+          </ul>
+        </nav>
+    <!-- end .panel_content --></div>
+  <!-- end .sidebar1 --></div>
+  <article class="content">
+    <h1>Manage your debt. Monitor your credit. Save with pre-qualified loans.</h1>
+	<section>
+		<p><img src="images/information.png" alt="info" /></p>
+        <p>Credit Sesame gives you the right tools to take control of your finances. Here are a few of the ways we help:</p><hr />
+    </section>
+
+    <section>
+      <div class="left_section">
+    	<img src="images/sect1.jpg" alt="sect1" />
+      </div>
+      <div class="right_section">
+         <h2>Stay On Top Of Your Finances – For Life</h2>
+          <p>We automatically track and monitor your loan and debt information then tell you how you’re doing. You’ll see your loans, debt and credit all in one place. You’ll know where your credit score stands every month, and understand how to improve your debt situation. Do you know how your finances measure up?</p>
+          <p><a href="#">Find out, for free</a></p>
+      </div>
+    </section>
+    
+    
+    <section>
+      <div class="left_section">
+    	<img src="images/sect2.jpg" alt="sect1" />
+      </div>
+      <div class="right_section">
+      	<h2>Rest Assured We’re Always Searching For You</h2>
+      	<p>We're like a personal shopper for better financing! We monitor the lending markets and track data from major banks every day. You can check back with us daily to find out if we've found ways for you to <a href="#">save</a></p>
+      </div>
+    </section>
+    
+    
+    <section>
+      <div class="left_section">
+    	<img src="images/sect3.jpg" alt="sect1" />
+      </div>
+      <div class="right_section">
+      	<h2>Rest Assured We’re Always Searching For You</h2>
+      	<p>We analyze every one of your current loans and debts against your income and credit profile. If there are better options out there, we'll find a way for you to save instantly. Is your debt optimized?</p>
+      	<p><a href="#">Find out today</a></p>
+      </div>
+    </section>
+    
+    
+    <section>
+      <div class="left_section">
+    	<img src="images/sect4.jpg" alt="sect1" />
+      </div>
+      <div class="right_section">
+      	<h2>&#39;What If&#39; Modeling To See How Things Change</h2>
+      	<p>Wondering how much you can save if you had a better credit score or a higher income? Our What If section lets you see how much you'll save and what you'll be prequalified for when your financial situation changes.</p>
+      </div>
+    </section>
+    
+    
+    <section>
+      <div class="left_section">
+    	<img src="images/sect5.jpg" alt="sect1" />
+      </div>
+      <div class="right_section">
+      	<h2>Apply For Loans Online – And Track Your Status</h2>
+      	<p>You can apply online through your secure login. We'll also update your application status to keep you posted.</p>
+      </div>
+    </section>
+ 
+ 
+     <section>
+      <div class="left_section">
+      	<!-- Nothing yet -->
+      </div>
+      <div class="right_section">
+      	<hr />
+      	<p><button class="sprites_button get_started">&nbsp;</button></p>
+      	<p>or learn about <a href="#">How it Works</a></p>
+      </div>
+    </section>
+    <!-- end .content --></article>
+    <div class="content_bottom">&nbsp;</div>
+</div>
+<div class="footer_container">
+  <footer>
+  	<nav>
+        <a href="/refinance/">Refinance</a> &nbsp;|&nbsp;      
+        <a href="/free-credit-score/">Free Credit Score</a> &nbsp;|&nbsp;    
+        <a href="/mortgage/" >Mortgage</a> &nbsp;|&nbsp;   
+        <a href="/debt/">Debt Management</a>
+    </nav>
+  	<nav>
+    	<a href="/">Home</a> &nbsp;|&nbsp;
+        <a href="/about/">About Us</a> &nbsp;|&nbsp;
+        <a href="/site-map/">Site Map</a> &nbsp;|&nbsp;
+        <a href="/how-we-help/faqs/">FAQ</a> &nbsp;|&nbsp;
+        <a href="/about/privacy-policy/">Privacy Policy</a> &nbsp;|&nbsp;
+        <a href="/about/terms-of-use/">Terms of Use</a> &nbsp;|&nbsp;
+        <a href="/how-we-help/faqs/">Help</a>
+    </nav>
+    <p>
+    <a href="https://trustsealinfo.verisign.com/splash?form_file=fdf/splash.fdf&dn=secure.creditsesame.com&lang=en" target="_blank">
+    	<img src="images/verisign.png" alt="verisign" title="Verified by VeriSign" />
+    <!-- START SCANALERT CODE --> 
+    <a target="_blank" href="https://www.mcafeesecure.com/RatingVerify?ref=secure.creditsesame.com">
+    	<img width="91" src="//images.scanalert.com/meter/secure.creditsesame.com/13.gif" alt="McAfee SECURE sites help keep you safe from identity theft, credit card fraud, spyware, spam, viruses and online scams" oncontextmenu="alert('Copying Prohibited by Law - McAfee SECURE is a Trademark of McAfee, Inc.'); return false;" />
+       </a> 
+    <!-- END SCANALERT CODE -->
+    </p>
+    <address>
+    	&copy; 2010 - 2011 Credit Sesame, Inc. &nbsp;|&nbsp;  All Rights Reserved.<br />
+		440 N Wolfe Rd. Sunnyvale, CA 94085
+    </address>
+  </footer>
+  <!-- end .container --></div>
+</body>
+</html>
---
+++ task1_HTML5-design-from-PSD_PHP/index.php
@@ -0,0 +1,1 @@
+<?php include_once('Templates/index.dwt.php'); ?>
