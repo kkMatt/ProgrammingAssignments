17e4c4786bece562a5f4ab4b36201bf151c294b5
Kestutis ITDev <KestutisIT@users.noreply.github.com>
2014-06-24 20:29:22 +0300

    Update template.index.php

--- activity_n1_extended_PHP_CODE_EXAMPLE/template.index.php
+++ activity_n1_extended_PHP_CODE_EXAMPLE/template.index.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Kestutis Matuliauskas - Nor1, programming assignment, #1 - Program</title>
+<title>Kestutis - Nor1, programming assignment, #1 - Program</title>
 <!-- stylesheets -->
 <link rel="stylesheet" href="css/styles.css" type="text/css">
 </head>
